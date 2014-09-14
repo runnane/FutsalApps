6e0a501d1da2037e2d803aeb7cde454d413c2b06
Jon Tungland <jon@tungland.org>
2014-09-14 14:25:21 +0200

    Initial commit

---
+++ TimeControl/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TimeControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimerText = new System.Windows.Forms.Label();
+            this.btnToggleTimer = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblRestTime = new System.Windows.Forms.Label();
+            this.chkAlwaysOnTop = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTimerText
+            // 
+            this.lblTimerText.AutoSize = true;
+            this.lblTimerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimerText.Location = new System.Drawing.Point(0, 9);
+            this.lblTimerText.Name = "lblTimerText";
+            this.lblTimerText.Size = new System.Drawing.Size(470, 108);
+            this.lblTimerText.TabIndex = 2;
+            this.lblTimerText.Text = "00:00:000";
+            // 
+            // btnToggleTimer
+            // 
+            this.btnToggleTimer.Location = new System.Drawing.Point(18, 111);
+            this.btnToggleTimer.Name = "btnToggleTimer";
+            this.btnToggleTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnToggleTimer.TabIndex = 3;
+            this.btnToggleTimer.Text = "Start";
+            this.btnToggleTimer.UseVisualStyleBackColor = true;
+            this.btnToggleTimer.Click += new System.EventHandler(this.btnToggleTimer_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(99, 111);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "9:00 - Initial",
+            "14:00 - Endgame",
+            "1:00 - Suspension",
+            "4:30",
+            "7:00",
+            "10:00",
+            "15:00"});
+            this.comboBox1.Location = new System.Drawing.Point(180, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // lblRestTime
+            // 
+            this.lblRestTime.AutoSize = true;
+            this.lblRestTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRestTime.Location = new System.Drawing.Point(15, 137);
+            this.lblRestTime.Name = "lblRestTime";
+            this.lblRestTime.Size = new System.Drawing.Size(143, 20);
+            this.lblRestTime.TabIndex = 6;
+            this.lblRestTime.Text = "Remaining time: ";
+            // 
+            // chkAlwaysOnTop
+            // 
+            this.chkAlwaysOnTop.AutoSize = true;
+            this.chkAlwaysOnTop.Location = new System.Drawing.Point(387, 137);
+            this.chkAlwaysOnTop.Name = "chkAlwaysOnTop";
+            this.chkAlwaysOnTop.Size = new System.Drawing.Size(92, 17);
+            this.chkAlwaysOnTop.TabIndex = 7;
+            this.chkAlwaysOnTop.Text = "Always on top";
+            this.chkAlwaysOnTop.UseVisualStyleBackColor = true;
+            this.chkAlwaysOnTop.CheckedChanged += new System.EventHandler(this.chkAlwaysOnTop_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(482, 14);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 166);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chkAlwaysOnTop);
+            this.Controls.Add(this.lblRestTime);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnToggleTimer);
+            this.Controls.Add(this.lblTimerText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(497, 204);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(497, 204);
+            this.Name = "MainForm";
+            this.Opacity = 0.95D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FutsalApps | TimeControl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimerText;
+        private System.Windows.Forms.Button btnToggleTimer;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblRestTime;
+        private System.Windows.Forms.CheckBox chkAlwaysOnTop;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ TimeControl/MainForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeControl
+{
+    public partial class MainForm : Form
+    {
+        internal DateTime start;
+        internal DateTime realStart;
+        internal bool paused;
+        internal DateTime pausedAt;
+
+        internal Color ToggleColor = Color.Yellow;
+        internal Color defaultColor = Color.LightGray;
+
+        internal TimeSpan roundTime
+        {
+            get
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+
+                    case 0:
+                        return new TimeSpan(0, 9, 00);
+                    case 1:
+                        return new TimeSpan(0, 14, 00);
+                    case 2:
+                        return new TimeSpan(0, 1, 00);
+                    case 3:
+                        return new TimeSpan(0, 4, 30);
+                    case 4:
+                        return new TimeSpan(0, 7, 00);
+                    case 5:
+                        return new TimeSpan(0, 15, 00);
+                    case 6:
+                        return new TimeSpan(0, 10, 00);
+                    default:
+                        return new TimeSpan(0, 1, 00);
+
+
+                }
+
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleTimer_Click(object sender, EventArgs e)
+        {
+
+            if (paused || timer1.Enabled)
+            {
+                timer1.Enabled = false;
+
+                timer1.Stop();
+                btnToggleTimer.Text = "Start";
+                btnPause.Enabled = false;
+                paused = false;
+                btnPause.Text = "Pause";
+                if (timer2.Enabled)
+                {
+                    timer2.Stop();
+                    this.BackColor = defaultColor;
+                }
+
+            }
+            else
+            {
+                timer1.Enabled = true;
+                realStart = start = DateTime.Now;
+                timer1.Start();
+                btnToggleTimer.Text = "Stopp";
+                btnPause.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan tsp = DateTime.Now - start;
+            lblTimerText.Text = tsp.Minutes.ToString("D2") + ":" + tsp.Seconds.ToString("D2") + ":" + tsp.Milliseconds.ToString("D3");
+            TimeSpan rtsp = (roundTime - tsp);
+            lblRestTime.Text = "Resterende tid: " + rtsp.Minutes.ToString("D2") + ":" + rtsp.Seconds.ToString("D2") + ":" + rtsp.Milliseconds.ToString("D3")
+                + "   (" + rtsp.Minutes.ToString("D2") + "m " + rtsp.Seconds.ToString("D2") + "s)";
+            if (rtsp.TotalSeconds < 0)
+            {
+                this.ToggleColor = Color.Red;
+                if (!timer2.Enabled)
+                    timer2.Start();
+            }
+            else if (rtsp.TotalSeconds < 30)
+            {
+                this.ToggleColor = Color.Yellow;
+                if (!timer2.Enabled)
+                    timer2.Start();
+            }
+            else
+            {
+                timer2.Stop();
+            }
+            //double res1 = ((roundTime.TotalSeconds - rtsp.TotalSeconds) / roundTime.TotalSeconds);
+            //int res = (int)res1*1000;
+            try
+            {
+                progressBar1.Maximum = (int)roundTime.TotalSeconds;
+                progressBar1.Value = (int)(roundTime.TotalSeconds - rtsp.TotalSeconds);
+                //progressBar1.Update();
+                progressBar1.Refresh();
+            }
+            catch (Exception)
+            {
+
+            }
+           // label1.Text = (res1*1000).ToString();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (!paused)
+            {
+                pausedAt = DateTime.Now;
+                timer1.Stop();
+                btnPause.Text = "Fortsett";
+                paused = true;
+                if (timer2.Enabled)
+                {
+                    timer2.Stop();
+                    this.BackColor = defaultColor;
+                }
+
+
+            }else{
+                TimeSpan tsp = DateTime.Now - pausedAt;
+                start = start.Add(tsp);
+
+                timer1.Start();
+                btnPause.Text = "Pause";
+                paused = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.BackColor = this.BackColor == this.ToggleColor ? defaultColor : this.ToggleColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            timer2.Interval = 250;
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void chkAlwaysOnTop_CheckedChanged(object sender, EventArgs e)
+        {
+
+            this.TopMost = chkAlwaysOnTop.Checked;
+        }
+
+
+    }
+}
